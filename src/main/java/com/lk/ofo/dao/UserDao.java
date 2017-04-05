366da29361f34e74f2b0e3cb2445f774c7b9ae16
lkj41110 <807637152@qq.com>
2017-04-05 11:33:52 +0800

    userDao

--- src/main/java/com/lk/ofo/dao/UserDao.java
+++ src/main/java/com/lk/ofo/dao/UserDao.java
@@ -8,39 +8,39 @@ import com.lk.ofo.entity.User;
 
 public interface UserDao {
 
-	/**
-	 * 通过id和密码查询管理员
-	 * 
-	 * @param name
-	 * @param password
-	 * @return
-	 */
-	User queryUserByPhoneAndPassword(@Param("phone") String phone, @Param("password") String password);
+    /**
+     * 通过id和密码查询管理员
+     *
+     * @param password
+     * @return
+     */
+    User queryUserByPhoneAndPassword(@Param("phone") String phone, @Param("password") String password);
 
-	/**
-	 * 查询全部
-	 * 
-	 * @param offset
-	 * @param limit
-	 * @return
-	 */
-	List<User> queryAllUser(@Param("offset") int offset, @Param("limit") int limit);
+    /**
+     * 查询全部
+     *
+     * @param offset
+     * @param limit
+     * @return
+     */
+    List<User> queryAllUser(@Param("offset") int offset, @Param("limit") int limit);
 
-	/**
-	 * 通过id找用户
-	 * @param id
-	 * @return
-	 */
-	User queryUserById(int id);
+    /**
+     * 通过id找用户
+     *
+     * @param id
+     * @return
+     */
+    User queryUserById(int id);
 
-	/**
-	 * 通过姓名找用户
-	 */
-	// TODO
+    /**
+     * 通过姓名找用户
+     */
+    // TODO
 
-	/**
-	 * 修改用户
-	 */
-	// TODO
+    /**
+     * 修改用户
+     */
+    Boolean update(User user);
 
 }
